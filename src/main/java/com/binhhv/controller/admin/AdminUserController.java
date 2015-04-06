cb24a8e639b122ca44c2d35783ff7e606d345f43
binhhv <hvbinh1990@gmail.com>
2015-04-06 11:54:18 +0700

    make it better

--- src/main/java/com/binhhv/controller/admin/AdminUserController.java
+++ src/main/java/com/binhhv/controller/admin/AdminUserController.java
@@ -1,5 +1,10 @@
 package com.binhhv.controller.admin;
 
-public class AdminUserController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+@RequestMapping(value={"/admin/","/admin"})
+public class AdminUserController {
+	
 }
